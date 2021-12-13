2e7d9a6bb24ff221ad6a5a93d071e04964599363
dprastha <dprastha@users.noreply.github.com>
2021-12-13 06:42:25 +0000

    deploy: c5c762218a64abb3822ecc0cfc37163e5e44c6f8

---
+++ precache-manifest.78ba3c56577cd8b6540a48041c474f30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10a849c6a1fedbdb23eeebd0133a08a",
+    "url": "https://dprastha.github.io/app-kuliner-frontend/index.html"
+  },
+  {
+    "revision": "422dad4b75382b41ad41",
+    "url": "https://dprastha.github.io/app-kuliner-frontend/static/css/main.7612875f.chunk.css"
+  },
+  {
+    "revision": "8bfb69e55ab71939ebb4",
+    "url": "https://dprastha.github.io/app-kuliner-frontend/static/js/2.b1059f2c.chunk.js"
+  },
+  {
+    "revision": "422dad4b75382b41ad41",
+    "url": "https://dprastha.github.io/app-kuliner-frontend/static/js/main.4fd0e9a5.chunk.js"
+  },
+  {
+    "revision": "b4f10e04c55256d87d95",
+    "url": "https://dprastha.github.io/app-kuliner-frontend/static/js/runtime~main.d2e2d797.js"
+  }
+]);
